f2d654223927516554bd1824460ac6bf7bfe4249
Lex Ark <dante54dmc@gmail.com>
2022-02-27 16:54:32 +0300

    ghost fade fix

--- Assets/Scripts/GhostFade.cs
+++ Assets/Scripts/GhostFade.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class GhostFade : MonoBehaviour {
 
-    private float alpha = 1f;
+    [SerializeField] private float alpha = 1f;
+    [SerializeField] private float speed = .015f;
 
     Renderer[] renderers;
 
@@ -15,21 +16,38 @@ public class GhostFade : MonoBehaviour {
         //}
     }
 
-    public void Fade(bool fade) {
-        StartCoroutine(IEFade(fade ? 0f : 1f));
+    [ContextMenu("Fade")]
+    public void Fade() {
+        StartCoroutine(IEFade(true));
     }
 
-    IEnumerator IEFade(float targetAlpha) {
+    [ContextMenu("Show")]
+    public void Show() {
+        StartCoroutine(IEFade(false));
+    }
+
+    IEnumerator IEFade(bool fade) {
+        float targetAlpha = fade ? 0f : 1f;
         while (alpha != targetAlpha) {
-            SetAlpha(Mathf.MoveTowards(alpha, targetAlpha, .02f));
+            SetAlpha(Mathf.MoveTowards(alpha, targetAlpha, speed));
             yield return new WaitForSeconds(.02f);
         }
+        SetShadow(!fade);
     }
 
     void SetAlpha(float alpha) {
         this.alpha = alpha;
         foreach (var renderer in renderers) {
             renderer.material.SetFloat("GhostAlpha", alpha);
+        }
+    }
+
+    void SetShadow(bool shadow) {
+        UnityEngine.Rendering.ShadowCastingMode mode = shadow 
+            ? UnityEngine.Rendering.ShadowCastingMode.On 
+            : UnityEngine.Rendering.ShadowCastingMode.Off;
+        foreach (var renderer in renderers) {
+            renderer.shadowCastingMode = mode;
         }
     }
 
